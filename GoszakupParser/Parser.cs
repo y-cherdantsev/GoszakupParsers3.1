625ee3fc0a9eae7d160f36e556a3fd6f4323b6dd
ZhekaCher <zhekacher20.05@gmail.com>
2020-02-25 12:09:10 +0600

    Modified

--- GoszakupParser/Parser.cs
+++ GoszakupParser/Parser.cs
@@ -1,19 +1,53 @@
-﻿namespace GoszakupParser
-{
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Security;
+using NLog;
 
+namespace GoszakupParser
+{
     /// @author Yevgeniy Cherdantsev
     /// @date 25.02.2020 10:53:43
     /// @version 1.0
     /// <summary>
-    /// INPUT
+    /// Parsing abstract class
     /// </summary>
-
-
     public abstract class Parser
     {
-        
+        private readonly Logger _logger; /*!< Логгер текущего класса */
+        protected string Url { get; set; }
+        private static int Total { get; set; }
+
+        public Parser()
+        {
+            _logger = InitLogger();
+        }
+
+        protected abstract Logger InitLogger();
         public abstract void Parse();
         public abstract void ProcessObject(object entity);
-        protected abstract string GetPageResponse(string url);
+
+        protected string GetPageResponse(string url)
+        {
+            var request = WebRequest.Create($"https://ows.goszakup.gov.kz/{url}?limit=500");
+            request.Method = WebRequestMethods.Http.Get;
+            request.Headers["Content-Type"] = "application/json";
+            request.Headers["Authorization"] = Configuration.AuthToken;
+            request.AuthenticationLevel = AuthenticationLevel.None;
+            var response = request.GetResponse();
+            var objReader =
+                new StreamReader(response.GetResponseStream() ?? throw new NullReferenceException());
+
+            var sLine = "";
+            var pageResponse = "";
+            while (sLine != null)
+            {
+                sLine = objReader.ReadLine();
+                if (sLine != null)
+                    pageResponse += sLine;
+            }
+
+            return pageResponse;
+        }
     }
 }
