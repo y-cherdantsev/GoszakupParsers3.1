208319fa4a183e30cff9572b26db7f83ab0977f9
ZhekaCher <yevgeniy.cherdantsev@gmail.com>
2020-06-16 09:44:34 +0600

    Model

--- GoszakupParser/Models/Dtos/PlanDto.cs
+++ GoszakupParser/Models/Dtos/PlanDto.cs
@@ -2,6 +2,53 @@
 {
     public class PlanDto
     {
-        
+        public string id { get; set; }
+        public string plan_act_id { get; set; }
+        public string plan_act_number { get; set; }
+        public string ref_plan_status_id { get; set; }
+        public string plan_fin_year { get; set; }
+        public string plan_preliminary { get; set; }
+        public string rootrecord_id { get; set; }
+        public string sys_subjects_id { get; set; }
+        public string subject_biin { get; set; } 
+        public string name_ru { get; set; }
+        public string name_kz { get; set; }
+        public string ref_trade_methods_id { get; set; }
+        public string ref_units_code { get; set; }
+        public string count { get; set; } 
+        public string price { get; set; }
+        public string amount { get; set; } 
+        public string ref_months_id { get; set; }
+        public string ref_pln_point_status_id { get; set; }
+        public string pln_point_year { get; set; }
+        public string ref_subject_types_id { get; set; }
+        public string ref_enstru_code { get; set; }
+        public string ref_finsource_id { get; set; }
+        public string ref_abp_code { get; set; }
+        public string date_approved { get; set; }
+        public string is_qvazi { get; set; } 
+        public string date_create { get; set; }
+        public string timestamp { get; set; }
+        public string system_id { get; set; }
+        public string ref_point_type_id { get; set; }
+        public string desc_ru { get; set; }
+        public string desc_kz { get; set; }
+        public string extra_desc_ru { get; set; }
+        public string extra_desc_kz { get; set; }
+        public string sum_1 { get; set; }
+        public string sum_2 { get; set; }
+        public string sum_3 { get; set; }
+        public string supply_date_ru { get; set; }
+        public string prepayment { get; set; }
+        public string ref_justification_id { get; set; }
+        public string ref_amendment_agreem_type_id { get; set; }
+        public string ref_amendm_agreem_justif_id { get; set; }
+        public string contract_prev_point_id { get; set; }
+        public string disable_person_id { get; set; }
+        public string transfer_sys_subjects_id { get; set; }
+        public string transfer_type { get; set; }
+        public string ref_budget_type_id { get; set; }
+        public string subject_name_kz { get; set; }
+        public string subject_name_ru { get; set; }
     }
 }
