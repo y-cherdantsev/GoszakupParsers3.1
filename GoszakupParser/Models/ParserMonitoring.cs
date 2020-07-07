53906f72732556076cbb82e6ff13b3ad0a35802a
Yevgeniy Cherdantsev <zhekacher20.05@gmail.com>
2020-07-07 14:13:27 +0600

    Refactorings

--- GoszakupParser/Models/ParserMonitoring.cs
+++ GoszakupParser/Models/ParserMonitoring.cs
@@ -4,16 +4,15 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace GoszakupParser.Models
 {
-	[Table("parser_monitoring")]
-	public class ParserMonitoring
-	{
-		[Key] [Column("id")] public int? Id{get; set;}
-		[Column("name")] public string Name{get; set;}
-		[Column("parsed")] public bool? Parsed{get; set;}
-		[Column("last_migrated")] public DateTime? LastMigrated{get; set;}
-		[Column("last_parsed")] public DateTime? LastParsed{get; set;}
-		[Column("owner")] public int? Owner{get; set;}
-		[Column("active")] public bool? Active{get; set;}
-		
-	}
+    [Table("parser_monitoring")]
+    public class ParserMonitoring
+    {
+        [Key] [Column("id")] public int? Id { get; set; }
+        [Column("name")] public string Name { get; set; }
+        [Column("parsed")] public bool? Parsed { get; set; }
+        [Column("last_migrated")] public DateTime? LastMigrated { get; set; }
+        [Column("last_parsed")] public DateTime? LastParsed { get; set; }
+        [Column("owner")] public int? Owner { get; set; }
+        [Column("active")] public bool? Active { get; set; }
+    }
 }
--- GoszakupParser/Models/Proxy.cs
+++ GoszakupParser/Models/Proxy.cs
@@ -5,16 +5,15 @@ using NpgsqlTypes;
 
 namespace GoszakupParser.Models
 {
-	[Table("proxies")]
-	public class Proxy
-	{
-		[Column("address")] public IPAddress Address{get; set;}
-		[Column("port")] public int Port{get; set;}
-		[Column("username")] public string Username{get; set;}
-		[Column("password")] public string Password{get; set;}
-		[Column("status")] public bool? Status{get; set;}
-		[Column("relevant_from")] public DateTime? RelevantFrom{get; set;}
-		[Column("relevant_until")] public DateTime? RelevantUntil{get; set;}
-		
-	}
+    [Table("proxies")]
+    public class Proxy
+    {
+        [Column("address")] public IPAddress Address { get; set; }
+        [Column("port")] public int Port { get; set; }
+        [Column("username")] public string Username { get; set; }
+        [Column("password")] public string Password { get; set; }
+        [Column("status")] public bool? Status { get; set; }
+        [Column("relevant_from")] public DateTime? RelevantFrom { get; set; }
+        [Column("relevant_until")] public DateTime? RelevantUntil { get; set; }
+    }
 }
