6b4582a6407ac8bc2b4c11a6d7ffe68a1cd77870
Yevgeniy Cherdantsev <yevgeniy.cherdantsev@gmail.com>
2020-08-14 14:04:29 +0600

    Refactored

--- GoszakupParser/Program.cs
+++ GoszakupParser/Program.cs
@@ -3,7 +3,6 @@ using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Sockets;
-using System.Runtime.InteropServices;
 using System.Text;
 using System.Text.Json;
 using CommandLine;
