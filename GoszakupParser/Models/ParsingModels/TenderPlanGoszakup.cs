1fc00c1467fad56edb5c4a5e05716a405d490991
Yevgeniy Cherdantsev <yevgeniy.cherdantsev@gmail.com>
2020-09-07 10:19:02 +0600

    Comment updated

--- GoszakupParser/Models/ParsingModels/TenderPlanGoszakup.cs
+++ GoszakupParser/Models/ParsingModels/TenderPlanGoszakup.cs
@@ -12,7 +12,7 @@ namespace GoszakupParser.Models.ParsingModels
     /// @author Yevgeniy Cherdantsev
     /// @date 26.02.2020 18:43:05
     /// <summary>
-    /// Plan Parsing DB table object
+    /// Tender Plan Parsing DB table object
     /// </summary>
     [Table("tender_plan_goszakup")]
     public class TenderPlanGoszakup : BaseModel
