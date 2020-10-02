114517463c5d0cbf8a8a9472d0a1cef743caeb42
Yevgeniy Cherdantsev <yevgeniy.cherdantsev@gmail.com>
2020-10-02 10:18:23 +0600

    Updated Dto classes naqmings

--- GoszakupParser/Models/Dtos/TrdBuyDto.cs
+++ GoszakupParser/Models/Dtos/TrdBuyDto.cs
@@ -20,45 +20,45 @@ namespace GoszakupParser.Models.Dtos
         public string startDate { get; set; }
         public string endDate { get; set; }
         public string publishDate { get; set; }
-        public TrdBuyType RefTypeTrade { get; set; }
-        public TrdBuyType RefTradeMethods { get; set; }
-        public TrdBuyType RefSubjectType { get; set; }
-        public TrdBuyType RefBuyStatus { get; set; }
-        public TrdBuyFile[] Files { get; set; }
-        public TrdBuyLot[] Lots { get; set; }
+        public TrdBuyTypeDto RefTypeTrade { get; set; }
+        public TrdBuyTypeDto RefTradeMethods { get; set; }
+        public TrdBuyTypeDto RefSubjectType { get; set; }
+        public TrdBuyTypeDto RefBuyStatus { get; set; }
+        public TrdBuyFileDto[] Files { get; set; }
+        public TrdBuyLotDto[] Lots { get; set; }
 
-        public class TrdBuyType
+        public class TrdBuyTypeDto
         {
             public string nameRu { get; set; }
         }
 
-        public class TrdBuyLot
+        public class TrdBuyLotDto
         {
             public string lotNumber { get; set; }
-            public TrdBuyType RefLotsStatus { get; set; }
+            public TrdBuyTypeDto RefLotsStatus { get; set; }
             public string customerBin { get; set; }
             public string descriptionRu { get; set; }
             public double amount { get; set; }
             public double count { get; set; }
             public string nameRu { get; set; }
-            public TrdBuyFile[] Files { get; set; }
+            public TrdBuyFileDto[] Files { get; set; }
             public TrdBuyPlan[] Plans { get; set; }
 
             public class TrdBuyPlan
             {
                 public string refEnstruCode { get; set; }
                 public string supplyDateRu { get; set; }
-                public TrdBuyType RefUnits { get; set; }
-                public TrdBuyPlanKato[] PlansKato { get; set; }
+                public TrdBuyTypeDto RefUnits { get; set; }
+                public TrdBuyPlanKatoDto[] PlansKato { get; set; }
 
-                public class TrdBuyPlanKato
+                public class TrdBuyPlanKatoDto
                 {
                     public string fullDeliveryPlaceNameRu { get; set; }
                 }
             }
         }
 
-        public class TrdBuyFile
+        public class TrdBuyFileDto
         {
             public string filePath { get; set; }
             public string originalName { get; set; }
